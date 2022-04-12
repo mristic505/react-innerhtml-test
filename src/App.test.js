5ead3ed59dd0d7f27b74eef081ebb68e3fe1925f
Mateja Ristic <matejaristic@MatejaR-USMAC22.local>
2022-04-12 08:09:45 -0400

    upgrade react 18

--- src/App.test.js
+++ src/App.test.js
@@ -1,14 +1,16 @@
 import React from "react";
+import App from "./App";
+const { TextEncoder, TextDecoder } = require("util");
 
+// Here we are NOT using ReactDOM anymore
+// import ReactDOM from "react-dom";
 
-import ReactDOM from "react-dom";
-
+// Switched to createRoot instead of ReactDOM
+import { createRoot } from 'react-dom/client';
 
 import { expect } from "chai";
 const { JSDOM } = require("jsdom");
 
-
-import App from "./App";
 
 describe("App Component Testing", () => {
     it("Renders Hello World Title", () => {
@@ -16,10 +18,11 @@ describe("App Component Testing", () => {
         global.window = dom.window;
         const element = dom.window.document.getElementById("test");                        
 
-        
-        ReactDOM.render(<App />, element); 
+        // Here we are NOT using ReactDOM anymore
+        // ReactDOM.render(<App />, element); 
 
-                
+        // Switched to createRoot instead of ReactDOM
+        createRoot(element).render(<App />);
                      
         expect(element.innerHTML).equals("<h1>Hello World</h1>");
     });
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import { StrictMode } from "react";
-import { render } from "react-dom";
+import { createRoot } from 'react-dom/client';
 
 import App from "./App";
 
 const rootElement = document.getElementById("root");
-render(<App />, rootElement );
+const root = createRoot(rootElement);
+root.render(<App />);
